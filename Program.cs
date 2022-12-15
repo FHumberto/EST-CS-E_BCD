4089a73b5fde2933b992efba879a3ccfa9592880
Humberto Guedes <franciscohguedes@gmail.com>
2022-12-15 16:48:07 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_BCD
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
